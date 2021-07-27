ff2713c6d7f7949284ba73304a5459239be7d383
Ganeshram Ravi <ganeshram.ravi@rrd.com>
2021-07-27 17:25:28 +0530

    Fixed Error

--- FC.Extension.SQL.Execution/PostgreSQL/PostgreSQLDataAccess_Execution.cs
+++ FC.Extension.SQL.Execution/PostgreSQL/PostgreSQLDataAccess_Execution.cs
@@ -30,10 +30,12 @@ namespace FC.Extension.SQL.Execution.PostgreSQL
                  .RuleFor(o => o.Name, f => f.Person.FullName)
                  .RuleFor(o => o.Email, f => f.Person.Email);
             var person = personFake.Generate();
-            SQLExtension.SQLConfig = new SQLExecutionConfig()
-            { Compiler = SQLCompiler.PostgreSQL, 
-                ConnectionString = "User Id=FCPos;Password=System@1234;Server=localhost;Port=5432;Database=ExtensionTest;", 
-                Trace = null };
+            SQLExtension.SQLConfig = new SQLConfig()
+            {
+                Compiler = SQLCompiler.PostgreSQL,
+                ConnectionString = "User Id=FCPos;Password=System@1234;Server=localhost;Port=5432;Database=ExtensionTest;",
+                Trace = null
+            };
             if (IsSave)
             {
                 person = person.Save().Result;
--- FC.Extension.SQL/Helper/SQLExecutionConfig.cs
+++ FC.Extension.SQL/Helper/SQLExecutionConfig.cs
@@ -7,21 +7,21 @@ namespace FC.Extension.SQL.Helper
     /// <summary>
     /// The Property used to conifgure SQL Execution commends.
     /// </summary>
-    public class SQLExecutionConfig
-    {
-        public string ConnectionString { get; set; }
-        public SQLCompiler Compiler { get; set; }
-        public BaseTrace Trace { get; set; }
-    }
+    //public class SQLExecutionConfig
+    //{
+    //    public string ConnectionString { get; set; }
+    //    public SQLEngine Compiler { get; set; }
+    //    public BaseTrace Trace { get; set; }
+    //}
 
-    /// <summary>
-    /// SQL Compiler used to choose the server
-    /// </summary>
-    public enum SQLCompiler
-    {        
-        SQLServer,
-        PostgreSQL,
-        MySQL,
-        SQLite
-    }
+    ///// <summary>
+    ///// SQL Compiler used to choose the server
+    ///// </summary>
+    //public enum SQLEngine
+    //{        
+    //    SQLServer,
+    //    PostgreSQL,
+    //    MySQL,
+    //    SQLite
+    //}
 }
