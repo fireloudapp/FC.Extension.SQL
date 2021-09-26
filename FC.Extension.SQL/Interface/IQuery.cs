ae5800fa0269d6f53993b8e0245654ad1241aba9
Ganeshram Ravi <sr.ganeshram@gmail.com>
2021-09-26 18:34:20 +0530

    Truncate and IQuery Interface Added

---
+++ FC.Extension.SQL/Interface/IQuery.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FC.Extension.SQL.Interface
+{
+    public interface IQuery<TModel>
+    {
+        string Message { get; set; }
+        IEnumerable<TModel> GetHandler(TModel model);
+        Task<IEnumerable<TModel>> GetHandlerAsync(TModel model);
+    }
+    public interface IQueryPaging<Paging>
+    {
+        Paging GetHandler(Paging model);
+        Task<Paging> GetHandlerAsync(Paging model);
+    }
+    public interface IQueryById<TModel>
+    {
+        TModel GetHandler(long id);
+        Task<TModel> GetHandlerAsync(long id);
+    }
+}
